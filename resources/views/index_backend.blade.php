88cb92b64462db813fe7de55b2b9e2669367e742
ddika990 <ddika990@gmail.com>
2018-09-29 16:20:55 +0700

    revisi template tidak boleh lebih dari 2 scroll

--- resources/views/index_backend.blade.php
+++ resources/views/index_backend.blade.php
@@ -11,14 +11,11 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('template/backend/matrix-admin-bt4/assets/images/favicon.png')}}">
     <title>Matrix Template - The Ultimate Multipurpose admin template</title>
-    <!-- Custom CSS -->
-    <link href="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/css/float-chart.css')}}" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="{{asset('template/backend/matrix-admin-bt4/dist/css/style.min.css')}}" rel="stylesheet">
+    @include('master_backend.css_index_backend')
     @yield('css')
 </head>
 
-<body oncontextmenu="return false" onkeydown="return false;" onmousedown="return false;">
+<body >
    {{-- oncontextmenu="return false" onkeydown="return false;" onmousedown="return false;" for disable inspect element with mouse --}}
    
     <div class="preloader">
@@ -31,7 +28,7 @@
     <div id="main-wrapper">
        
         <header class="topbar" data-navbarbg="skin5">
-            @include('master_backend.topnav')
+            @include('master_backend.topnav_index')
         </header>
     
         <aside class="left-sidebar" data-sidebarbg="skin5">
@@ -39,53 +36,13 @@
         </aside>
        
         <div class="page-wrapper">
-          
              @yield('content')
-          
-            <footer class="footer text-center">
-                All Rights Reserved by Matrix-admin. Designed and Developed by <a href="https://wrappixel.com">WrapPixel</a>.
-            </footer>
-            
+            @include('master_backend.footer')           
         </div>
-
     </div>
-   
-    @yield('js')
 
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/jquery/dist/jquery.min.js')}}"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/extra-libs/sparkline/sparkline.js')}}"></script>
-    <!--Wave Effects -->
-    <script src="{{asset('template/backend/matrix-admin-bt4/dist/js/waves.js')}}"></script>
-    <!--Menu sidebar -->
-    <script src="{{asset('template/backend/matrix-admin-bt4/dist/js/sidebarmenu.js')}}"></script>
-    <!--Custom JavaScript -->
-    <script src="{{asset('template/backend/matrix-admin-bt4/dist/js/custom.min.js')}}"></script>
-    <!--This page JavaScript -->
-    <!-- <script src="../../dist/js/pages/dashboards/dashboard1.js"></script> -->
-    <!-- Charts js Files -->
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/excanvas.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/jquery.flot.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/jquery.flot.pie.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/jquery.flot.time.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/jquery.flot.stack.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot/jquery.flot.crosshair.js')}}"></script>
-    <script src="{{asset('template/backend/matrix-admin-bt4/assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js')}}"></script>
-    <script src="{{asset('matrix-admin-bt4/dist/js/pages/chart/chart-page-init.js')}}"></script>
-    <script type="text/javascript">
-        function document.onkeydown() 
-    { 
-        if ( event.keyCode==17) //17 is ascii code for ctrl
-        { 
-            event.keyCode = 0; 
-            event.cancelBubble = true; 
-            return false; 
-        } 
-    }
-    </script>
+    @yield('js')
+    @include('master_backend.js_index_backend')
 </body>
 
 </html>
