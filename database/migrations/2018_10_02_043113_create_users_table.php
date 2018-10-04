b58e57ff144ad775baa2001d016d602291734c6f
rifaldi <ar440499@gmail.com>
2018-10-04 11:49:52 +0700

    migration

--- database/migrations/2018_10_02_043113_create_users_table.php
+++ database/migrations/2018_10_02_043113_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('id_user');
             $table->string('nama_user');
-            $table->string('gmail')->unique();
+            $table->string('email')->unique();
             $table->string('password');
+            $table->string('status');
             $table->integer('saldo')->nullable();
-            $table->enum('status',['user','admin'])
+            $table->enum('level',['user','admin']);
             $table->timestamps();
         });
     }
