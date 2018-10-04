f205ebf535ec29e8beb0e01c25cde3e2c0593c83
Syaikhul <msasyaikhul@gmail.com>
2018-10-04 16:43:03 +0700

    proses_login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use Input;
 class LoginController extends Controller
 {
     /*
@@ -37,5 +42,72 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    
+    function login(request $request)
+    {
+        $rules = array(
+            'email' => 'required|email',
+            'password' => 'required|min:4'
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->route('dashboard.index')->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+
+            $credentials = [
+                'email' => Input::get('email'),
+                'password' => Hash::make($request->password),
+            ];
+            
+            dd(Auth::attempt($credentials));
+
+            if (Auth::attempt($userdata)) {
+                dd('sukses', $request->all());
+                // validation successful
+                // do whatever you want on success
+
+            } else {
+                dd('gagal auth', $request->all());
+
+                    // validation not successful, send back to form
+
+                // return redirect()->route('dashboard.index');
+            }
+        }
+    }
+
+    public function logout(Request $request)
+    {
+      // dd($request->name);
+        $ids = auth::user()->lock;
+        if ($ids == 'login') {
+        // dd('salah');
+            $data = User::where('id', auth::user()->id)->first();
+            $data->lock = 'logout';
+            $data->save();
+
+        // dd(auth::user()->name);
+        // dd($data);
+            $this->guard()->logout();
+
+            $request->session()->flush();
+
+            $request->session()->regenerate();
+
+            return redirect('/login')
+                ->withSuccess('Terimakasih, selamat datang kembali!');
+        } else {
+
+            $this->guard()->logout();
+
+            $request->session()->flush();
+
+            $request->session()->regenerate();
+
+            return redirect('/login')
+                ->withSuccess('Terimakasih, selamat datang kembali!');
+        }
+    }
 }
