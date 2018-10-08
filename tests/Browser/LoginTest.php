e401b49f748989aa33c436d5b3f731ed9f6173b2
Syaikhul <msasyaikhul@gmail.com>
2018-10-08 14:56:13 +0700

    laravel dusk instal

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function ($first, $second) {
+            $first->loginAs(User::find(1))
+                ->visit('/home');
+        });
+        
+    }
+}
