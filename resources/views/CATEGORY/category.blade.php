2262c4ca61323bb27ba0c86c96da38b87760a504
rifaldi <ar440499@gmail.com>
2018-10-19 13:08:04 +0700

    category

--- resources/views/CATEGORY/category.blade.php
+++ resources/views/CATEGORY/category.blade.php
@@ -1,60 +1,61 @@
 <!DOCTYPE html>
  <html>
+
  <style>
     table, th, td {
         border: 1px solid black;
     }
  </style>
- <head>
-     <meta charset="utf-8" />
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <title>category barang</title>
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-     <script src="main.js"></script>
- </head>
+
+     <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>category barang</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+        <script src="main.js"></script>
+     </head>
  <body>
 
      <h1> CATEGORY BARANG </h1>
      
   <table style="width:100%">
- <tr>
-    <th>id</th>
-    <th>Kode_Barang</th>
-    <th>Nama_Barang</th>
-    <th>Dibuat </th>
-    <th>Diperbarui</th>
-    <th>Delete</th>
- </tr>
+     <tr>
+            <th>id</th>
+            <th>Kode_Barang</th>
+            <th>Nama_Barang</th>
+            <th>Dibuat </th>
+            <th>Diperbarui</th>
+            <th>Delete</th>
+     </tr>
  @foreach($data as $index => $data)
- <tr>    
-        <td>{{ $data->id }}</td>
-        <td>{{ $data->kode_kategori }}</td>
-        <td>{{ $data->nama_kategori }}</td>
-        <td>{{ $data->created_at }}</td>
-        <td>{{ $data->updated_at }}</td>
-
-     <td>
-  <form action="{{route('category.destroy', $data->id)}}" method="post">
-  {{csrf_field()}}
-  <input type="hidden" name="_method" value="DELETE" />
-  <button type="submit">Delete</button>
-  </form>
-  <a href="{{route('category.edit', $data->id)}}">Edit</a>
-     </td>
- </tr>
+     <tr>    
+            <td>{{ $data->id }}</td>
+            <td>{{ $data->kode_kategori }}</td>
+            <td>{{ $data->nama_kategori }}</td>
+            <td>{{ $data->created_at }}</td>
+            <td>{{ $data->updated_at }}</td>
+         <td>
+            <form action="{{route('category.destroy', $data->id)}}" method="post">
+            {{csrf_field()}}
+            <input type="hidden" name="_method" value="DELETE" />
+            <button type="submit">Delete</button>
+            </form>
+            <a href="{{route('category.edit', $data->id)}}">Edit</a>
+         </td>
+     </tr>
  @endforeach
   </table> 
 
         <form action="{{route('category.store')}}" method="post">
-        @csrf
-        KODE CATEGORY:<br>
-        <input type="number" name="kode_category" >
-        <br>
-        NAMA BARANG:<br>
-        <input type="text" name="name_category" >
-        <br><br>
-        <input type="submit" value="Submit">
+            @csrf
+            KODE CATEGORY:<br>
+            <input type="number" name="kode_category" >
+            <br>
+            NAMA BARANG:<br>
+            <input type="text" name="name_category" >
+            <br><br>
+            <input type="submit" value="Submit">
         </form> 
 
  </body>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('UpayUwin/User', function(){
 });
 
 Route::get('UpayUwin', function(){
+<<<<<<< Updated upstream
     return view('frontend.index');
 })->name('awal');
 
@@ -30,6 +31,13 @@ Route::get('UpayUwin/cart', function(){
 
 Route::get('UpayUwin/product', function(){
     return view('frontend.product');
+=======
+	return view('index_frontend');	
+});
+
+Route::get('/', function(){
+	return view('welcome');	
+>>>>>>> Stashed changes
 });
 
 Route::get('UpayUwin/payment', function(){
@@ -38,23 +46,38 @@ Route::get('UpayUwin/payment', function(){
 
 Route::get('/pay','PaymentController@payWithpaypal');
 
+<<<<<<< Updated upstream
 Route::get('/backend/login',function(){
 	return view('backend.User.login');
 });
 //Route Untuk Backend
 Route::prefix('backend')->middleware('admin')->group(function () {
 
+=======
+Route::resource('category','categoryController');
+
+//Route Untuk Backend
+Route::prefix('backend')->group(function () { 
+	
+	//Route Dashboard
+>>>>>>> Stashed changes
 	Route::resource('dashboard','backend\DashboardController');
-	//Route Voucher
-	Route::get('voucher/kode','backend\VoucherController@cek_kode')->name('cek.kode');
+	//Rsoute Dashboard
 	Route::resource('voucher','backend\VoucherController');
 	//Route User
+<<<<<<< Updated upstream
 	Route::resource('user','backend\UserController');
 	//Route setting
 	Route::resource('setting2','backend\SettingController');
   Route::get('/settingweb', 'backend\SettingController@setting_web')->name('settingweb');
   	//Route setting
   	Route::resource('setting','backend\SettingController');
+=======
+	Route::prefix('User')->group(function () {
+		
+
+	});
+>>>>>>> Stashed changes
 });
 
 
@@ -64,9 +87,13 @@ Route::prefix('frontend')->group(function () {
 	//Route wallet
 	Route::prefix('wallet')->group(function () {
 	});
-
+    
 });
 
+<<<<<<< Updated upstream
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/formLogin', 'Auth\LoginController@showLoginForm')->name('formLogin');
+=======
+Route::resource('category','categoryController');
+>>>>>>> Stashed changes
