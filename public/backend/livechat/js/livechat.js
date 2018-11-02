9a73f09e100ed39a0545cbe2cd7210081695ca0a
Velent Fernando <velentfernando@gmail.com>
2018-11-02 15:14:50 +0700

    Strange Push

--- public/backend/livechat/js/livechat.js
+++ public/backend/livechat/js/livechat.js
@@ -7,41 +7,70 @@ var config = {
   storageBucket: "ecommerce-upayuwin.appspot.com",
   messagingSenderId: "179852905701"
 };
-
-//Send Chat onKey Enter
-var send = document.getElementById('chat-input');
-            var date = new Date().toLocaleString();
-            send.addEventListener("keyup",function(event){
-                event.preventDefault();
-                if (event.keyCode == 13) {
-                // console.log(date)
-                alert(this.value)
-                this.value = "";
-                }
-            })
-            
+function scrolling()
+{
+  var objDiv = document.getElementById("chatroom");
+  objDiv.scrollTop = objDiv.scrollHeight;
+}
+           
 var db = firebase.initializeApp(config).database();
 var chatRef = db.ref('livechat/users/');
 var users = "";
 chatRef.on('child_added', function showData(items)
 {
-    
-     users +=       "<div class='chat_list' onclick='clickme(this)'><div class='chat_people'><div class='chat_img'><img src='https://ptetutorials.com/images/user-profile.png' alt='sunil'></div><div class='chat_ib'><h5>"+items.key+"</h5><h4><span class='chat_date'>Dec 25</span></h4><p>Test, which is a new approach to have all solutions astrology under one roof.</p></div></div></div>";
-     $('#myDIV').html(users);
+      console.log(items.key)
+      var chatRef3 = db.ref('livechat/users/'+items.key);
+     chatRef3.orderByValue().limitToLast(1).on('child_added',function showData(items)
+      {
+        lastmsguser = items.val().message;
+      });
+      users +=       "<div class='chat_list' onclick='clickme(this)'><div class='chat_people'><div class='chat_img'><img src='https://ptetutorials.com/images/user-profile.png' alt='sunil'></div><div class='chat_ib'><h5>"+items.key+"</h5><h4><span class='chat_date'>Dec 25</span></h4><p id='lastmsg"+items.key+"'>"+lastmsguser+"</p></div></div></div>";
+     
+      console.log(lastmsguser);
+      $('#myDIV').html(users);
      
 })
 
 var Title = '';
 function clickme(elem) {
   var Title = $('h5', elem).text();
-  alert(Title)
   // get all 'a' elements
   var a = document.getElementsByClassName("chat_list");
   // loop through all 'a' elements
   for (i = 0; i < a.length; i++) {
       // Remove the class 'active' if it exists
       a[i].classList.remove('active_chat')
+      $('#chat-input').prop("disabled", false);
   }
   // add 'active' classs to the element that was clicked
   elem.classList.add('active_chat');
+  var msg ='';
+  var chatRef2 = db.ref('livechat/users/'+Title);
+  chatRef2.on('child_added',function showData(items)
+			{
+                if (items.val().pembalas == 'admin') {
+                    msg += "<div class='outgoing_msg'><div class='sent_msg'><p>"+ items.val().message +"</p><span class='time_date'>"+ items.val().waktu +"</span> </div></div>";
+                } 
+                   else {
+                    msg +="<div class='incoming_msg'><div class='incoming_msg_img'> <img src='https://ptetutorials.com/images/user-profile.png' alt='sunil'> </div><div class='received_msg'><div class='received_withd_msg'><p>"+ items.val().message +"</p><span class='time_date'>"+ items.val().waktu +"</span></div></div></div>";
+                }
+                $('#chatroom').html(msg);	
+      });
+      scrolling();
+      //Send Chat onKey Enter
+      var send = document.getElementById('chat-input');
+      var date = new Date().toLocaleString();
+      send.addEventListener("keyup",function(event){
+          event.preventDefault();
+          if (event.keyCode == 13) {
+          // console.log(date)
+          chatRef2.push({
+              message : this.value,
+              pembalas : "admin",
+              status : "unread",
+              waktu : date
+          })
+          this.value = "";
+          }
+      })
 }
--- resources/views/backend/customer_service/index.blade.php
+++ resources/views/backend/customer_service/index.blade.php
@@ -59,49 +59,12 @@
         <div class="mesgs">
           <div class="msg_history" id="chatroom">
                   
-            <!-- <div class="incoming_msg">
-              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>Test which is a new approach to have all
-                    solutions</p>
-                  <span class="time_date"> 11:01 AM    |    June 9</span></div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Test which is a new approach to have all
-                  solutions</p>
-                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-            </div>
-            <div class="incoming_msg">
-              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>Test, which is a new approach to have</p>
-                  <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date"> 11:01 AM    |    Today</span> </div>
-            </div>
-            <div class="incoming_msg">
-              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford.</p>
-                  <span class="time_date"> 11:01 AM    |    Today</span></div>
-              </div>
-            </div> -->
+             
 
           </div>
           <div class="type_msg">
             <div class="input_msg_write">
-              <input type="text" class="write_msg" placeholder="Write a Reply..." id="chat-input" autocomplete="off" />
+              <input disabled="disabled" onkeyup="myFunction()" type="text" class="write_msg" placeholder="Write a Reply..." id="chat-input" autocomplete="off" />
               </div>
           </div>
         </div>
@@ -112,15 +75,6 @@
       
     </div>
 </div>
-<script>
-$(document).on('ready', function(){
-                        console.log('ready');
-                        var objDiv = document.getElementById("chatroom");
-                        objDiv.scrollTop = objDiv.scrollHeight;
-                })
-
-
-</script>
 <script  src="{{asset('backend/livechat')}}/js/livechat.js"></script>
     
 @endsection
