e8698b43c90a9d2266f3044d3e1c8d8371748a9e
ddika990 <ddika990@gmail.com>
2018-11-06 06:42:06 +0700

    update image firebase
    gagal

--- resources/views/backend/item/additionalbitem.blade.php
+++ resources/views/backend/item/additionalbitem.blade.php
@@ -1,75 +1,6 @@
 @section ('css')
-	<style>
-		.labels{
-			margin-top: 8px;
-		}
-		.add-button{
-			margin-bottom: 10px;
-		}
-		.closebtn {
-		    margin-left: 15px;
-		    font-weight: bold;
-		    float: right;
-		    font-size: 22px;
-		    line-height: 20px;
-		    cursor: pointer;
-		    transition: 0.3s;
-		}
-		.fotoAdd,.foto{
-			margin-top: 10px;
-			width: 100%;
-			height: 100%;
-		}
-	</style>
 @endsection
-
 @section ('js')
-	<script>
-		$(document).ready(function() {
-			var save = true;
-			$('#label_danger').hide();
-			var data = $.parseJSON($.ajax({
-				url: '{{Route('cek.kode')}}',
-				type: 'GET',
-    			dataType: 'json',
-    			 async: false
-			}).responseText);
-
-		// checking the voucher code if it has same value
-		$('#code').keyup(function(event) {
-
-			$.each(data, function(index, val) {
-
-					if($('#code').val() == val.kode_voucher){
-						save = false;
-						$('#label_danger').show();
-						$('#code').addClass('is-invalid');
-						return false;
-					}else{
-						save = true;
-						$('#label_danger').hide();
-						$('#code').removeClass('is-invalid');
-					}
-
-				});
-			});
-			$(document).on('submit','#form_add',function(){
-				if (save == false) {
-					event.preventDefault();
-         			toastr.error('Tidak dapat melanjutkan kode voucher sudah ada.', 'Danger!');
-				}
-			});
-			//Display image on change input
-			$("#input_logo").change(function() {
-			  	readURL(this);
-			});
-			$(".input_logo").change(function() {
-				let key = $(this).attr('key');
-			  	readURL_edit(this,key);
-			});
-		});
-	</script>
-	@include('backend.item.more-js')
 	<script src="https://www.gstatic.com/firebasejs/5.5.4/firebase.js"></script>
 	<script type="text/javascript" src="{{asset('js/firebase-price_list.js')}}"></script>
 	<script type="text/javascript">
@@ -100,7 +31,7 @@
 	        		<td>'+ value.stok +'</td>\
 	        		<td><img src="'+value.gambar+'" height="100" width="100"/></td>\
 	        		<td><button type="button" class="btn btn-outline-warning updateData" data-toggle="modal" data-target="#Modal-edit" data-id="'+index+'"><i class="fas fa-pencil-alt"></i></button>\
-							<button type="button" class="btn btn-outline-danger removeData" data-toggle="modal" data-target="#Modal-delete" data-id="'+index+'"><i class="fas fa-trash-alt"></i></button></tr>');
+							<button type="button" class="btn btn-outline-danger removeData" data-toggle="modal" id="viewmodaledit" data-target="#Modal-delete" data-id="'+index+'"><i class="fas fa-trash-alt"></i></button></tr>');
 	    	}
 	    	lastIndex = index;
 	    });
@@ -112,7 +43,9 @@
 	var fileButton = document.getElementById("fileButton");
   fileButton.addEventListener('change', function(e){
   var file = e.target.files[0];
-  var storageRef = firebase.storage().ref('item/' + file.name);
+	var idimage = parseInt(lastIndex) + 1;
+	var rename = file.name + idimage;
+  var storageRef = firebase.storage().ref('item/' + rename);
     	var task = storageRef.put(file);
 			task.on('state_changed',
 			function progress(snapshot){
@@ -122,7 +55,6 @@
 					alert("sukses");
 					storageRef.getDownloadURL().then(function(url){
 						downloadURL = url;
-
 						$('#saveadd').on('click', function(){
 							var values = $("#form_add").serializeArray();
 							var code_item 			= values[0].value;
@@ -153,7 +85,8 @@
 			},
 		 );
   });
-	
+
+
 	//remove data
 	$("body").on('click', '.removeData', function() {
 		var id = $(this).attr('data-id');
@@ -202,43 +135,94 @@
 					</div>\
 					 <div class="col-md-9">\
 							<label class="labels">Foto :</label>\
+							<progress id="uploaderupdate" value="0" max="100">0%</progress>\
 							<input id="image_item" type="hidden" name="image" class="form-control" required>\
 							<input id="fileButtonupdate" type="file" name="image" class="form-control nameimage" required>\
 					</div>\
 			</div>';
 			$('#resultupdate').html(updateData);
 		});
+
 		//upload image update
+		var uploaderupdate = document.getElementById("uploaderupdate");
 		var fileButtonupdate = document.getElementById("fileButtonupdate");
-		console.log(fileButtonupdate);
-	  fileButtonupdate.addEventListener('change', function(e){
-	  var fileupdate = e.target.files['#fileButtonupdate'];
-	  var storageRefupdate = firebase.storage().ref('item/' + fileupdate);
-	    	storageRefupdate.put(fileupdate);
-	  });
+		fileButtonupdate.addEventListener('change', function(e){
+		var fileupdate = e.target.files['#fileButtonupdate'];
+		console.log(fileupdate);
+		var storageRefupdate = firebase.storage().ref('item/' + fileupdate.name);
+				var taskupdate = storageRefupdate.put(fileupdate);
+				taskupdate.on('state_changed',
+				function progress(snapshot){
+					var percentageupdate=(snapshot.bytesTransferred / snapshot.totalBytes)*100;
+					uploaderupdate.value=percentageupdate;
+					if (percentageupdate==100) {
+						alert("sukses");
+						storageRefupdate.getDownloadURL().then(function(url){
+							downloadURLupdate = url;
+							$('.saveupdate').on('click', function() {
+								var values = $("#modal-update").serializeArray();
+								var image_item 	= downloadURLupdate;
+								// console.log(image_item);
+								var postData = {
+									 kode				: values[0].value,
+									 nama				: values[1].value,
+									 kategori		: values[2].value,
+									 harga			: values[3].value,
+									 stok				: values[4].value,
+									 deskripsi	: values[5].value,
+									 gambar			: image_item,
+								};
+
+								var updates = {};
+								updates['item/' + updateID] = postData;
+
+								firebase.database().ref().update(updates);
+								event.preventDefault()
+								$("#Modal-edit").modal('hide');
+							});
+
+						});
+					}
+				},function error(err){
+					alert("gagal")
+				},
+			 );
+		});
+
+		// upload image update
+		// var fileButtonupdate = document.getElementById("fileButtonupdate");
+		// console.log(fileButtonupdate);
+	  // fileButtonupdate.addEventListener('change', function(e){
+	  // var fileupdate = e.target.files['#fileButtonupdate'];
+	  // var storageRefupdate = firebase.storage().ref('item/' + fileupdate);
+	  //   	storageRefupdate.put(fileupdate);
+	  // });
 	});
 
-	$('.saveupdate').on('click', function() {
-		var values = $("#modal-update").serializeArray();
-		var image_item 	= $('#fileButtonupdate').val().split('\\').pop();
-		// console.log(image_item);
-		var postData = {
-			 kode				: values[0].value,
-			 nama				: values[1].value,
-			 kategori		: values[2].value,
-			 harga			: values[3].value,
-			 stok				: values[4].value,
-			 deskripsi	: values[5].value,
-			 gambar			: image_item,
-		};
+	// $('.saveupdate').on('click', function() {
+	// 	var values = $("#modal-update").serializeArray();
+	// 	var image_item 	= downloadURLupdate;
+	// 	// console.log(image_item);
+	// 	var postData = {
+	// 		 kode				: values[0].value,
+	// 		 nama				: values[1].value,
+	// 		 kategori		: values[2].value,
+	// 		 harga			: values[3].value,
+	// 		 stok				: values[4].value,
+	// 		 deskripsi	: values[5].value,
+	// 		 gambar			: image_item,
+	// 	};
+	//
+	// 	var updates = {};
+	// 	updates['item/' + updateID] = postData;
+	//
+	// 	firebase.database().ref().update(updates);
+	// 	event.preventDefault()
+	// 	$("#Modal-edit").modal('hide');
+	// });
 
-		var updates = {};
-		updates['item/' + updateID] = postData;
 
-		firebase.database().ref().update(updates);
-		event.preventDefault()
-		$("#Modal-edit").modal('hide');
-	});
+
 
 	</script>
 
