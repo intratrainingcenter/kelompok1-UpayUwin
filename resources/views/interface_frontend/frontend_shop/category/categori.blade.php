f43f31b436671410c8caf40a3a2a662f73efb14b
Syaikhul <msasyaikhul@gmail.com>
2018-11-05 11:12:20 +0700

    perbaikan link

--- resources/views/interface_frontend/frontend_shop/category/categori.blade.php
+++ resources/views/interface_frontend/frontend_shop/category/categori.blade.php
@@ -41,13 +41,17 @@
                         @if ($product == 'voucher')
                         <a href="{{url('/voucher/'.$cat->id)}}"><img class="full-width img-responsive" src="{{asset('FrontEnd')}}/assets/img/blog/16.jpg" alt=""></a>
                         @else
-                        <a href="{{url('/item_category/'.$cat->nama_kategori)}}"><img class="full-width img-responsive" src="{{asset('FrontEnd')}}/assets/img/blog/16.jpg" alt=""></a>
+                        <a href="{{url('/item/'.$cat->nama_kategori)}}"><img class="full-width img-responsive" src="{{asset('FrontEnd')}}/assets/img/blog/16.jpg" alt=""></a>
                         @endif
                     </div>
                     <div class="product-description product-description-brd">
                         <div class="overflow-h ">
                             <div class="pull-left">
-                                <h4 class="title-price"><a href="{{url('/voucher/'.$cat->id)}}">{{$cat->nama_kategori}}</a></h4>
+                                @if ($product == 'voucher')
+                                    <h4 class="title-price"><a href="{{url('/voucher/'.$cat->id)}}">{{$cat->nama_kategori}}</a></h4>
+                                @else
+                                    <h4 class="title-price"><a href="{{url('/item/'.$cat->nama_kategori)}}">{{$cat->nama_kategori}}</a></h4>
+                                @endif
                             </div>
                         </div>
                     </div>
